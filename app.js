12a7e7e1334a211d8854eabae184b4059a20c799
myst0123 <alfredo12393@yahoo.com>
2014-04-27 19:53:09 -0700

    changed callback

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/auth/facebook', function(req, res) {
 	if (!req.query.code) {
 		var authUrl = graph.getOauthUrl({
 			'client_id': process.env.facebook_client_id,
-			'redirect_uri': 'http://localhost:3000/auth/facebook',
+			'redirect_uri': 'http://facebirthday.herokuapp.com/auth/facebook',
 			'scope': 'user_friends, friends_birthday'//you want to update scope to what you want in your app
 		});
 
@@ -46,7 +46,7 @@ app.get('/auth/facebook', function(req, res) {
 	}
 	graph.authorize({
 		'client_id': process.env.facebook_client_id,
-		'redirect_uri': 'http://localhost:3000/auth/facebook',
+		'redirect_uri': 'http://facebirthday.herokuapp.com/auth/facebook',
 		'client_secret': process.env.facebook_client_secret,
 		'code': req.query.code
 	}, function( err, facebookRes) {
